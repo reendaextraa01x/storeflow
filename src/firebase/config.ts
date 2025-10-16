15781631a6352e46e0f947dfc2cf8f35f6ef56ba
reendaextra.a extra <reendaextraa01@gmail.com>
2025-10-16 02:54:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7558013324-552f3",
+  "appId": "1:1025819949469:web:3424fc611cc57dc540d774",
+  "apiKey": "AIzaSyA12u7f0xmpZyEOse1a7sHjyLg3k-W0eg4",
+  "authDomain": "studio-7558013324-552f3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1025819949469"
+};
