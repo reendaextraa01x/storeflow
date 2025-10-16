de6bb52f6876315c396cc278a1ff9e6cb3c82bfc
reendaextra.a extra <reendaextraa01@gmail.com>
2025-10-16 03:49:47 +0000

    mude o desing do modo escuro

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,30 +1,9 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
-function ThemeEffect() {
-  const { theme } = useTheme();
-
-  React.useEffect(() => {
-    const body = document.body;
-    body.classList.remove('simple-dark');
-
-    if (theme === 'dark') {
-      // Use um nome de classe específico para o tema escuro simples
-      body.classList.add('simple-dark');
-    }
-  }, [theme]);
-
-  return null;
-}
-
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return (
-    <NextThemesProvider {...props}>
-      <ThemeEffect />
-      {children}
-    </NextThemesProvider>
-  )
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
