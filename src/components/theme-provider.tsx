104701bb48f23aa2ff6f84d5e36ff959c45174f9
reendaextra.a extra <reendaextraa01@gmail.com>
2025-10-16 03:47:12 +0000

    o modo escuro e sistema são o mesmo tema

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,9 +1,29 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
+function ThemeEffect() {
+  const { theme } = useTheme();
+
+  React.useEffect(() => {
+    const body = document.body;
+    body.classList.remove('super-dark');
+
+    if (theme === 'dark') {
+      body.classList.add('super-dark');
+    }
+  }, [theme]);
+
+  return null;
+}
+
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider {...props}>
+      <ThemeEffect />
+      {children}
+    </NextThemesProvider>
+  )
 }
